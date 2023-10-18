4df1b860690d05536e23d48f1a404c86137dcb1a
FRESHMEMBA <mazibukosibonelo@gmail.com>
2023-10-18 22:24:17 +0200

    Simple shell project

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /*
@@ -7,6 +6,22 @@
  */
 int main(void)
 {
-	prompt();
+	char *pntr;	/*Pointer to the string containing the user input*/
+	size_t len = 0;	/*Length of the string pointed to by pntr*/
+	ssize_t n;	/*Number of characters read from stdin*/
+
+	while (true)
+	{
+		prompt();
+		n = getline(&pntr, &len, stdin);
+		/*Exits the program if getline returned -1*/
+		if (n == -1)
+		{
+			return (-1);
+		}
+
+		free(pntr);
+	}
+
 	return (0);
 }
--- main.h
+++ main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
 void prompt(void);
 
 #endif /*MAIN_H*/
